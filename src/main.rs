0f5a3659fa708b0e869a3d92bd4bbb26cdba24c4
Cyrille Blanchet <cyrille.blanchet@gmail.com>
2019-10-27 14:08:29 +0100

    struct to count folders and files

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 use std::fs;
 use std::io;
+use std::ffi::OsString;
 use std::path::{Path,PathBuf};
 use std::time::SystemTime;
 
-//stocker dans une structure ou un hashmap
 struct Fichier
 {
-    /*create : SystemTime,
-    modify : SystemTime,*/
-    path : Box<PathBuf>,
-    prop : fs::Metadata,
+    modify : SystemTime,
+    len : u64,
+    name : OsString,
 }
 
 impl Fichier{
     fn new(p : &Path)->Option<Fichier>
     {
-        //let dir = *path.path();
-        let md = p.metadata();
-        match md
+        let m : SystemTime;
+        let l : u64;
+        let n : OsString;
+        n = ((*p).file_name().unwrap()).to_os_string();//why should it failed ?
+        let metadata = p.metadata();
+        match metadata
         {
-            Err(e) =>  None,
-            Ok(m) => {
-                let pb:PathBuf = (*p).to_path_buf();
-                let bp = Box::new( pb );
-                Some(Fichier{path: bp, prop:m})
+            Err(e) =>  {
+                println!("Error with metadata of {:?} -> {}",p,e); //appears on files of which i have no access right
+                None
+                },
+            Ok(md) => {
+                l = md.len();
+                m = md.modified().unwrap(); //What OS doesn't support modification time of a file ?
+                Some(Fichier{
+                    modify : m,
+                    len : l,
+                    name : n
+                    })
             }
         }
     }
@@ -31,14 +40,24 @@ impl Fichier{
 
 struct Dossier
 {
-    fichiers : Vec<Fichier>,
-    dossiers : Vec<Dossier>,
+    name : OsString,            //folder's name (only it : not the full path!!)
+    fichiers : Vec<Fichier>,    //files inside the folder
+    dossiers : Vec<Dossier>,    //sub-filders
 }
 
 impl Dossier{
-    fn new()->Dossier
+    fn new(dir : &Path)->Dossier
     {
-        Dossier{fichiers:Vec::new(),dossiers:Vec::new()}
+        let n = 
+        match (*dir).file_name()
+        {
+            Some(n) => n.to_os_string(), // pour un dossier ou un fichier quelconque => son nom
+            None => (*dir).as_os_str().to_os_string(),  //pour / ou c:\ ...   on garde tout
+        };
+        Dossier{
+            name : n, //(*dir.file_name().unwrap()).to_os_string(), //will fail on "." and ".."
+            fichiers:Vec::new(),
+            dossiers:Vec::new()}
     }
 
     fn add_dossier(&mut self, doss : Dossier)
@@ -52,66 +71,77 @@ impl Dossier{
     }
 }
 
-// one possible implementation of walking a directory only visiting files
-fn visit_dirs(dir: &Path,total_dir: &mut u32,total_fic: &mut u32,doss : &mut Dossier ){
-    let mut nb_dir=0;
-    let mut nb_fic=0;
-    if dir.is_dir() {
-        let dir_content = fs::read_dir(dir);
-        match dir_content 
-        {
-            Err(e) => println!("Error {} on {:?}",e,dir),
-            Ok(content) => {
-                for entry in content {
-                    match entry
-                    {
-                        Err(e) => println!("Error {}",e),
-                        Ok(e) => {
-                            let path = e.path();
-                            if path.is_dir() {
-                                nb_dir +=1;
-                                let mut sous_doss = Dossier::new();
-                                visit_dirs(&path,total_dir,total_fic,&mut sous_doss);
-                                doss.add_dossier(sous_doss);
-                            } else {
-                                nb_fic +=1;
-                                let fic = Fichier::new(&path);
-                                if fic.is_some()
-                                {
-                                    doss.add_fichier(fic.unwrap());
-                                }
-                                /*
-                                let md = dir.metadata();
-                                match md
-                                {
-                                    Err(e) =>  println!("Error {}",e),
-                                    Ok(m) => {
-                                        //date création 
-                                        let date_creation:SystemTime;
-                                        if let Ok(date_creation) = m.created() {
-                                        } else {
-                                            println!("Not supported on this platform");
-                                        }
-                                        let date_modif:SystemTime;
-                                        if let Ok(date_modif) = m.modified() {
-                                        } else {
-                                            println!("Not supported on this platform");
-                                        }
-                                        let fsize:u64 = m.len();
+struct Explore
+{
+    folder_explored_count: u32,
+    folder_forbidden_count: u32,
+    file_explored_count: u32,
+    file_forbidden_count: u32,   
+}
+
+impl Explore{
+    fn new()->Explore
+    {
+        Explore{
+            folder_explored_count: 0,
+            folder_forbidden_count: 0,
+            file_explored_count: 0,
+            file_forbidden_count: 0,   
+        }
+    }
+
+    fn run(&mut self,dir: &Path) -> Dossier
+    {
+        let mut d = Dossier::new(dir);
+        self.run_int(dir,&mut d);
+        d
+    }
+
+    //internal function called by run so run could do some 1st call things without testing at each runs
+    fn run_int(&mut self,dir: &Path,doss : &mut Dossier)
+    {
+        //count for this level of folder
+        if dir.is_dir() {
+            let dir_content = fs::read_dir(dir);
+            match dir_content 
+            {
+                Err(e) => {
+                    println!("Error {} on {:?}",e,dir);//appears on folder of which i have no access right
+                    self.folder_forbidden_count +=1;
+                    }, 
+                Ok(content) => {
+                    for entry in content {
+                        match entry
+                        {
+                            Err(e) => println!("Error {}",e),
+                            Ok(e) => {
+                                let path = e.path();
+                                if path.is_dir() {
+                                    self.folder_explored_count += 1;
+                                    let mut sous_doss = Dossier::new(&path);
+                                    self.run_int(&path,&mut sous_doss);
+                                    doss.add_dossier(sous_doss);
+                                } else {
+                                    self.file_explored_count += 1;
+                                    let fic = Fichier::new(&path);
+                                    if fic.is_some()
+                                    {
+                                        doss.add_fichier(fic.unwrap());
                                     }
-                                } */
+                                    else
+                                    {
+                                        self.file_forbidden_count +=1;
+                                    }
+                                }
                             }
-                        }
-                    };
+                        };
+                    }
                 }
             }
         }
     }
-    //println!("{:?} contient {} dossiers et {} fichiers",dir,nb_dir,nb_fic);
-    *total_dir = *total_dir + nb_dir;
-    *total_fic = *total_fic + nb_fic;
-    //Ok(())
 }
+
 
 fn pause()
 {
@@ -125,19 +155,14 @@ fn pause()
 
 fn main() {
     pause();
-
     let start = SystemTime::now();
-    let mut total_dir = 0;
-    let mut total_fic = 0;
-    let mut doss = Dossier::new();
     let root = Path::new("c:\\");
-    visit_dirs(root,&mut total_dir,&mut total_fic,&mut doss);
+    let mut explore = Explore::new();
+    let _data = explore.run(&root);
     let end = SystemTime::now();
     println!("Duration: {:?}", end.duration_since(start).expect("Error computing duration!")  );
-    println!("Total {} dir && {} files",total_dir,total_fic);
-
+    println!("Total {}/{} dir && {}/{} files",explore.folder_explored_count,explore.folder_forbidden_count,explore.file_explored_count,explore.file_forbidden_count);
     pause();
-
     //pour synch_1:
     //premier parcours de C:\ Dureation: 582.7216589s (affichage de chaque dossier avec nb dir et nb fic)
     //second parcours de C:\ (bénéficiant du cache) : 35.7666452s comptage ne fonctionne pas
@@ -154,4 +179,9 @@ fn main() {
     //4eme  296Ko -> 62 032K Duration: 44.2833532s Total 56470 dir && 294322 files
     //correction de bug (je passait toujours le même chemin a Fichier::new()  
     //5ème  276Ko -> 71 512K Duration: 39.9544583s Total 56472 dir && 294361 files
+
+    //pour syynch_3
+    //on ne gardant que nom,  taille et date modif dans Fichier
+    //316Ko -> 34 028Ko Duration: 42.6894236s Total 56472 dir && 294360 files
+    //320kO -> 38 392kO Duration: 49.4006888s Total 56478/60 dir && 294515/476 files
 }
